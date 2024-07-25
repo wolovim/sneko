32fc7aa68843140812a56a7e6e05375a15a9c77d
wolovim <3621728+wolovim@users.noreply.github.com>
2024-07-25 15:46:33 -0400

    feat: erc20 snekmate, input args, styling

--- src/sneko/snippets/script2.py
+++ src/sneko/snippets/script2.py
@@ -1,16 +1,4 @@
-# from web3 import Web3, EthereumTesterProvider
-#
-#
-# w3 = Web3(EthereumTesterProvider())
-#
-# # provide `constructor` args if appropriate:
-# deploy = w3.eth.contract(abi=ABI, bytecode=BYTECODE).constructor().transact()
-# contract_address = w3.eth.get_transaction_receipt(deploy)["contractAddress"]
-# contract = w3.eth.contract(address=contract_address, abi=ABI)
-#
-# # result = contract.functions.exampleFunction().call()
-
-
+import asyncio
 import os
 from web3 import AsyncWeb3, WebSocketProvider
 
